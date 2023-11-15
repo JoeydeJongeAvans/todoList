2b5283b7bdabccaff5e3777095463b4b3c3c3579
joeyd <70515106+JoeydeJongeAvans@users.noreply.github.com>
2023-11-15 13:17:01 +0100

    Backend werkt nu

---
+++ backend/ToDoApi/Controllers/TodoController.cs
@@ -0,0 +1,98 @@
+// In de Controllers-folder, maak een nieuw bestand TodoController.cs aan.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApi.Data;
+using ToDoApi.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TodoController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public TodoController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Todo
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
+    {
+        return await _context.Todos.ToListAsync();
+    }
+
+    // GET: api/Todo/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Todo>> GetTodoById(long id)
+    {
+        var todo = await _context.Todos.FindAsync(id);
+
+        if (todo == null)
+        {
+            return NotFound();
+        }
+
+        return todo;
+    }
+
+    // POST: api/Todo
+    [HttpPost]
+    public async Task<ActionResult<Todo>> PostTodo(Todo todo)
+    {
+        _context.Todos.Add(todo);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetTodoById), new { id = todo.Id }, todo);
+    }
+
+    // PUT: api/Todo/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTodo(long id, Todo todo)
+    {
+        if (id != todo.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(todo).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.Todos.Any(t => t.Id == id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // DELETE: api/Todo/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodo(long id)
+    {
+        var todo = await _context.Todos.FindAsync(id);
+        if (todo == null)
+        {
+            return NotFound();
+        }
+
+        _context.Todos.Remove(todo);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
--- backend/ToDoApi/Data/AppDbContext.cs
+++ backend/ToDoApi/Data/AppDbContext.cs
@@ -1,14 +1,15 @@
-// In Data/AppDbContext.cs
-
 using Microsoft.EntityFrameworkCore;
-using ToDoApi.Models;  // Make sure this using directive is present
+using ToDoApi.Models;
 
 namespace ToDoApi.Data
 {
     public class AppDbContext : DbContext
     {
-        // Your DbContext code here...
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
+        // Your DbSet and other configurations...
         public DbSet<Todo> Todos { get; set; }
     }
 }
